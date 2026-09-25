1d84423b49cb7b8f0d373bade8b7ae1770914ce2
alfredouser <aaaaaaaaaaaaaaa999999999999999@atomicmail.io>
2026-09-25 16:50:36 +0000

    chore: start Oxlint migration

---
+++ oxlint.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@fullstacksjs/oxlint-minimal';
+
+export default defineConfig({
+  modules: {
+    nodejs: true,
+    react: true,
+    nextjs: true,
+    vitest: true,
+  },
+});
